33438cad12c117eedb48f1eee8ddd75f84604421
Koraboevvv <bexruzkarabaev@gmail.com>
2025-05-12 00:58:44 +0500

    fff

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // {
+//     // M-1
 //     function kv(arr) {
 //     return arr.map((i) => i **2 )
 // }
@@ -27,6 +28,56 @@
 // }
 
 // {
+//     // M-2
+//     function nameArr(arr) {
+//         return arr.map( i => "Mr." + i )        
+//     }
+//     console.log(nameArr(["John","Akmal"]));
+//     function name(arr) {
+//         let newArr = []
+//         arr.forEach( i => newArr.push("Mr." + arr[i]))
+//         return newArr;
+//     };
+//     {
+//         const nameArr = arr => {
+//         let newArr = []
+//         for (let i = 0; i < arr.length; i++) 
+//             newArr.push("Mr." + arr[i]);
+//             return newArr
+//         };
+//     }
+// }
+
+{
+    // M-3
+    // function toqNumbers(arr) {
+    //     return arr.filter((i) => i % 2 === 1)
+    // }
+    // console.log(toqNumbers([1,2,3,4,5,6]));
+    // function toqNumbers(arr) {
+    //     let newArr = []
+    //     arr.forEach((i) => {
+    //             if (i % 2 === 1 ) 
+    //             newArr.push(i);
+    //     })
+    //     return newArr
+    // }
+    // {
+    //         const toqNumbers = arr => {
+    //     let newArr = []
+    //     for (let i = 0; i < arr.length; i++) {
+    //         if (arr[i] % 2 === 1 ) {
+    //             newArr.push(arr[i])
+    //         };
+    //     }
+    //     return newArr
+    // }
+    // }
+}
+
+
+// {
+//     // M-4
 //     function number(arr)  {
 //         let newArr = []
 //         let index = 0
@@ -56,25 +107,32 @@
 // }
 
 
-{
-    function letter(arr) {
-        return arr.map(i => i.length);
-    }
-    console.log(letter(["lorem","hi"]));
-    function letter(arr) {
-        let newArr = []
-        arr.forEach((i) => {
-            newArr.push(i.length)
-        });
-        return newArr
-    }
-        console.log(letter(["lorem","hi"]));
-    function letter(arr) {
-        let newArr = []
-        for (let i = 0; i < arr.length; i++) {
-            newArr.push(arr[i].length);
-        }
-        return newArr
-    }
-        console.log(letter(["lorem","hi"]));
-}
+// {
+//     // M-5
+//     function letter(arr) {
+//         return arr.map(i => i.length);
+//     }
+//     console.log(letter(["lorem","hi"]));
+//     function letter(arr) {
+//         let newArr = []
+//         arr.forEach((i) => {
+//             newArr.push(i.length)
+//         });
+//         return newArr
+//     }
+//         console.log(letter(["lorem","hi"]));
+//     function letter(arr) {
+//         let newArr = []
+//         for (let i = 0; i < arr.length; i++) {
+//             newArr.push(arr[i].length);
+//         }
+//         return newArr
+//     }
+//         console.log(letter(["lorem","hi"]));
+//     function letter(arr) {
+//         let newArr = []
+//         for (let i = 0; i < arr.length; i++) 
+//             newArr.push(arr[i].length )
+//         return newArr
+//     }
+// }
